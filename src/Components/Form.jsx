687d82b778581c4b19df0ea544f06e3537dd934a
hannahf86 <hannahfeehan.design@gmail.com>
2023-06-28 21:11:14 +0100

    error handling and began success message

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 const Form = () => {
-
-  // TODO: FINISH SUBMIT | ERROR HANDLING
-
   
   // Formik Logics
   const formik = useFormik({
@@ -22,34 +21,38 @@ const Form = () => {
 
     // Submit Form
     onSubmit:(values) => {
-
+      <Link to='/Success'></Link>
     }
-  })
+  });
 
   const styles = {
-    text: 'text-[#36384E] font-semibold mb-4 text-sm',
-    input: 'border rounded-xl border-[#242742] py-4 px-4 w-full mb-8',
+    input: 'border rounded-xl border-[#242742] p-4 w-full mb-8 mt-4',
     button: 'bg-gradient-to-r from-pink-500 to-[#FF6257] p-4 rounded-xl w-full text-white font-bold',
+    error: 'text-red-600 font-bold mb-4 text-md text-center m-4'
   }
 
 
   return (
     <div>
 
-      <p className={styles.text}>Email address</p>
-
       <form>
+        <label className= {`text-[#36384E] font-semibold pb-2 text-sm ${formik.touched.email && formik.errors.email} ? "text-red-500" : " " ` }>
+          { formik.errors.email ? formik.errors.email : " Email address "}
+        </label>
         <input 
           className={ styles.input }
-          type="text" 
+          type="email" 
           placeholder='Enter your email here' 
-          id='email' 
+          name='email' 
           required
           value={ formik.values.email }
           onChange = { formik.handleChange }
-          onSubmit = { formik.handleSubmit }
+          onBlur = { formik.handleBlur }
         />
-        <button type='submit' className={styles.button}>Subscribe to monthly newsletter</button>
+        <button 
+          type='submit' 
+          className={styles.button} 
+          onSubmit = { formik.handleSubmit }>Subscribe to monthly newsletter</button>
       </form>
 
     </div>
